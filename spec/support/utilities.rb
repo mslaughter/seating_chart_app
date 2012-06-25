649a3eaddae6f2550a8466196553268231b75d84
mslaughter <melissaslaughter@me.com>
2012-06-25 16:58:01 -0400

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,10 @@
-def full_title(page_title)
-  base_title = "Seating Chart App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
+include ApplicationHelper
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
 end
