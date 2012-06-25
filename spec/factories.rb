4107a0a2bf53f794b63d012fd4d63b330db4f779
mslaughter <melissaslaughter@me.com>
2012-06-25 13:09:16 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Melissa Slaughter"
+    email     "melissa@example.com"
+    password  "foobar"
+    password_confirmation  "foobar"
+  end
+end
