2b8317ff4403c1caf3b5f6396a3a497c1b143058
mslaughter <melissaslaughter@me.com>
2012-06-25 10:38:51 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,31 +3,38 @@ require 'spec_helper'
 describe "StaticPages" do
   
   let(:base_title) { "Seating Chart App" }
+  subject { page }
   
   describe "Home page" do
+    before { visit root_path }
     
-    it "should have the content 'Seating Chart App'" do
-      visit '/static_pages/home'
-      page.should have_content('Seating Chart App')
-    end
+    it { should have_selector('h1', text: 'Seating Chart App') }
+    it { should have_selector('h1', text: full_title('')) }
     
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', text: "#{base_title}")
-    end
   end
   
   describe "Help page" do
+    before { visit help_path }
     
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', text: 'Help')
-    end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
     
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', text: "#{base_title} | Help")
-    end
+  end
+  
+  describe "About page" do
+    before { visit about_path }
+    
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: full_title('About')) }
+
+  end
+  
+  describe "Contact page" do
+    before { visit contact_path }
+    
+    it { should have_selector('h1', text: 'Contact') }    
+    it { should have_selector('title', text: full_title('Contact')) }
+    
   end
 end
 
